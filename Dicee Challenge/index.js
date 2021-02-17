0be6fa35ec56f9bb91bd7acccf5e7865a9ea0011
Ali <65456557+Kim-Lee-Lin@users.noreply.github.com>
2021-02-17 21:34:13 +0900

    Dice Challange

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,33 @@
+      // first one
+
+var randomNumber1  = Math.floor(Math.random() * 6) + 1;
+
+var randomImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSourse = "images/"  + randomImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSourse);
+
+
+// second one
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSourse2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSourse2);
+
+
+// function
+
+if (randomNumber1 > randomNumber2) {
+
+document.querySelector("h1").innerHTML = "Player #1 wins!";
+
+}
+ else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player #2 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!!";
+}
